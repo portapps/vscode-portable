2df90eed802116ecb2d760e7e9b40105d02a5d9e
CrazyMax <crazy-max@users.noreply.github.com>
2018-09-26 09:50:49 +0200

    Typo

--- res/code.cmd
+++ res/code.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 
-set VSCODE_APPDATA=%~dp0\data\appdata
-set VSCODE_LOGS=%~dp0\data\logs
-set VSCODE_EXTENSIONS=%~dp0\data\extensions
+set VSCODE_APPDATA=%~dp0data\appdata
+set VSCODE_LOGS=%~dp0data\logs
+set VSCODE_EXTENSIONS=%~dp0data\extensions
 
 set VSCODE_DEV=
 set ELECTRON_RUN_AS_NODE=1
